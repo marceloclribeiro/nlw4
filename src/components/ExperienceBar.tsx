a9611f1bcd7aebae0b00c5eb64db43e0b6699d91
Marcelo Ribeiro <marcelittle@pop-os.localdomain>
2021-02-25 11:42:47 -0300

    aula-1

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,15 @@
+export function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <p>0 xp</p>
+      <div>
+        <div style={{ width: "50%" }} />
+
+        <p className="current-experience" style={{ left: "50%" }}>
+          300 xp
+        </p>
+      </div>
+      <p>600 xp</p>
+    </header>
+  );
+}
